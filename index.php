c0377d4d2f69f292259791f0fe4c70238bda773e
Richie Hughes <me@richiehughes.co.uk>
2015-12-22 21:13:34 +0000

    removed old code & changed so it returns json

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
-
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
 $app->group('/v1', function() use($app) {
-    // Instantiate the postcode class
-//    $postcode = new Postcode();
-
     // API Endpoint for our postcodes
     $app->get('/postcodes/{postcode}', function ($request, $response, $args) {
         $postcodeData = PostcodeDatabase::getPostcodeData($args['postcode']);
-        var_dump(json_encode($postcodeData));
-//        return $response->write(json_encode($postcodeData))
-//                ->withHeader('Content-Type', 'application/json');
+        return $response->write(json_encode($postcodeData))
+                ->withHeader('Content-Type', 'application/json');
     });
 });
 
